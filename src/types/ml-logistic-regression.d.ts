aec87bf2a17cc96769612f145943e2f1abd181cd
Ryan <ryanmcintyre@Ryans-MacBook-Air.local>
2025-12-19 15:47:25 -0500

    fix: type ml-logistic-regression default export

--- src/types/ml-logistic-regression.d.ts
+++ src/types/ml-logistic-regression.d.ts
@@ -1,1 +1,7 @@
-declare module 'ml-logistic-regression';
+declare module 'ml-logistic-regression' {
+    export default class LogisticRegression {
+        constructor(options?: unknown);
+        train(X: unknown, y: unknown): void;
+        predict(X: unknown): unknown;
+    }
+}
