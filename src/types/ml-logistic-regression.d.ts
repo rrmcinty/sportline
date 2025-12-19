978d20bc9d4709c59c513ab8eb65f0b7187b36bd
Ryan <ryanmcintyre@Ryans-MacBook-Air.local>
2025-12-19 15:51:40 -0500

    style: format ml-logistic-regression shim

--- src/types/ml-logistic-regression.d.ts
+++ src/types/ml-logistic-regression.d.ts
@@ -1,7 +1,7 @@
 declare module 'ml-logistic-regression' {
-    export default class LogisticRegression {
-        constructor(options?: unknown);
-        train(X: unknown, y: unknown): void;
-        predict(X: unknown): unknown;
-    }
+  export default class LogisticRegression {
+    constructor(options?: unknown);
+    train(X: unknown, y: unknown): void;
+    predict(X: unknown): unknown;
+  }
 }
